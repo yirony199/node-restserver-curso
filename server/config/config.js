9a3766be55bc8b046e142fe28f811677581cf502
Marcos Barrientos <soporte@in-contacto.com>
2018-12-15 08:47:43 -0500

    se hacen mejoras de configuracion

--- server/config/config.js
+++ server/config/config.js
@@ -22,7 +22,7 @@ let urlDB;
 if(process.env.NODE_ENV  == 'dev'){
     urlDB = "mongodb://localhost:27017/cafe";
 }else{
-    urlDB = "mongodb://cafe-user:incontacto123456@ds157493.mlab.com:57493/cafe";
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.urlDBN = urlDB;
